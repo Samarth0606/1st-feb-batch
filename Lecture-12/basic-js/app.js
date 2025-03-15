3b03be3f123a6d8a2ee0560189b20c2f67d484ec
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-03-15 21:16:22 +0530

    lecture 12 js starts

---
+++ Lecture-12/basic-js/app.js
@@ -0,0 +1,26 @@
+
+// let abc = 10
+// let sam = true
+// console.log(abc)
+// console.log(sam)
+
+// -------------------------
+
+
+// let samSAM = 10;
+// let sam0606 = 200;
+// // let 0606sam = true ❌
+// // let for = true ❌
+
+// let sam_vohra = 10;
+// let _sam = 100;
+// let sam$vohra = 200;
+// let $sam = 400;
+
+// let _ = 90;
+// let $ = 60;
+
+// -------------------------
+
+
+
---
+++ Lecture-12/string/app.js
@@ -0,0 +1,98 @@
+
+// let namee = "Samarth Vohra"
+
+// let city = 'Delhi'
+
+// let profession = `Mentor`
+
+
+// let ans = typeof(namee)
+// let ans2 = typeof(city)
+// let ans3 = typeof(profession)
+// console.log( ans );
+// console.log( ans2 );
+// console.log( ans3 );
+
+// -------------------------------------
+
+// concatenation
+
+// let namee = "Samarth Vohra"
+// let city = 'Delhi'
+// let profession = `Mentor`
+
+// output: Hey i am samarth vohra, i am a mentor from delhi
+
+// decent way
+// let out = "Hi i am " + namee + " I am a " + profession + " from " + city
+// console.log(out);
+
+// ----------------------------------------
+// best way - string template literals
+
+// let namee = "Samarth Vohra"
+// let city = 'Delhi'
+// let profession = `Mentor`
+
+// let out = `Hey I am ${namee} i am a ${profession} from ${city}`
+// console.log(out);
+
+// -----------------------------------------
+
+// methods of string
+
+// let var1 = "I am Samarth Vohra";
+// let var2 = "    I am Samarth Vohra    ";
+
+// let ans1 = var1.length; //property
+// let ans2 = var2.length;
+
+// console.log(ans1);
+// console.log(ans2);
+
+// ---------------------
+
+// let var3 = "i am SamaRth VohRA";
+
+// console.log( var3.toUpperCase() );
+
+// console.log( var3.toLowerCase() );
+ 
+// ---------------------
+// let var1 = "I am Samarth Vohra";
+// let var2 = "    I am Samarth Vohra    ";
+
+// let ans1 = var1.length; //property
+// let ans2 = var2.length;
+// let ans3 = var2.trim().length;
+
+// console.log(ans1);
+// console.log(ans2);
+// console.log(ans3);
+
+// ----------------------
+
+let var1 = "hi i am";
+let var2 = " samarth vohra";
+
+console.log( var1.concat(var2) );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
