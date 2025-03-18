a04c2e949ea943e254f2267679344578c43c4417
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-03-18 20:28:39 +0530

    lecture 13 commit

---
+++ Lecture-13/if-else/app.js
@@ -0,0 +1,50 @@
+
+// let age = 55;
+// let age = prompt("enter the age please");
+
+// if(age>=25){
+//     console.log("peele peele oo more raja");   
+// }
+// else if(age>=18){
+//     console.log("kundi mat khadkao raja");   
+// }
+// else{
+//     console.log("chala jaa brother");
+// }
+
+// --------------------------------------------
+
+
+// let marks = 43;
+// let att = 80;
+
+// if(marks>=40 && att>=75){
+//     console.log("pappu paas hogya");   
+// }else{
+//     console.log("pappu cant pass sala"); 
+// }
+
+// -------------------------------------------
+
+// short circuiting
+
+// let marks = 33;
+// // let att = 80;
+
+// if(marks>=40 && att>=75){
+//     console.log("pappu paas hogya");   
+// }else{
+//     console.log("pappu cant pass sala"); 
+// }
+
+// -------------------------------------------
+
+let marks = 43;
+// let att = 80;
+
+if(marks>=40 || att>=75){
+    console.log("pappu paas hogya");   
+}else{
+    console.log("pappu cant pass sala"); 
+}
+
---
+++ Lecture-13/numbers/app.js
@@ -0,0 +1,86 @@
+
+
+
+// // let b = 10.5;
+// let a = 10;
+// let c = 2;
+
+// console.log( typeof(a) );
+// console.log( typeof(c) );
+
+
+// // + , - , * , / , % , **
+
+// console.log( a + c );//12
+// console.log( a - c );//8
+// console.log( a * c );//20
+// console.log( c / a );// 2/10 = 0.2 
+// console.log( a ** c );// 10**2 = 100
+// console.log( a % c );// 
+
+// BODMAS ❌
+// PEMDAS ✅
+
+// ------------------------------------------
+
+
+// console.log(Math);
+
+// console.log(Math.PI); //property
+// console.log(Math.SQRT2); // property
+
+
+// console.log( Math.floor(5.5) ); //methods
+
+// console.log( Math.ceil(5.5) ); //mnethods
+
+// console.log( Math.round(5.5) ); //mnethods
+// console.log( Math.round(5.4) ); //mnethods
+// console.log( Math.pow(10,3)); //mnethods
+
+// console.log( Math.min(10,3,100,50)); //mnethods
+// console.log( Math.max(10,3,100,50)); //mnethods
+
+// --------------------------------------------
+
+
+// console.log(Math.random());
+// // console.log(Math.random());
+// console.log(Math.random()*10);
+
+
+
+// console.log(Math.random()*61 + 6);
+
+// console.log( Math.floor(Math.random()*61) );
+
+
+
+// console.log( Math.floor(Math.random()*900000) + 100000 );
+
+
+// ------------------------------------------------------------------
+
+// let a = 20;
+// let b = 10;
+
+
+// console.log( a > b  );
+// console.log( a < b  );
+
+// console.log( a <= b  );
+// console.log( a >= b  );
+
+// ! -> negation
+// console.log( !true  );
+// console.log( !false  );
+
+// ------------------------------------------------------------------
+
+let a = 10
+let b = "10"
+
+console.log( a==b);  // general equality -> type is not checked
+console.log( a===b); // strict equality  -> type is checked
+
+
