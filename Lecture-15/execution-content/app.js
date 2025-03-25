c8b58263c73314c5ef873420102ef07b67058f37
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-03-25 21:50:34 +0530

    lecture 15 commit

---
+++ Lecture-15/execution-content/app.js
@@ -0,0 +1,47 @@
+
+// var a = 10;
+// console.log(a);
+
+// function sam(){
+//     console.log("i am sam"); 
+// }
+// sam();
+
+
+// ---------------------------------
+
+
+// console.log(a);
+// var a = 100;
+// console.log(a);
+
+
+// sam();
+// function sam(){
+//     var c = true;
+//     console.log(c);
+// }
+
+
+// ---------------------------------
+
+
+// console.log(a);
+// abc();
+
+// var a = true;
+// function abc(){
+//     console.log("i am abc"); 
+// }
+
+
+// ---------------------------------
+console.log(a);
+
+abc();
+let a = true;
+
+function abc(){
+    console.log("i am abc"); 
+}
+
---
+++ Lecture-15/let_var_const/app.js
@@ -0,0 +1,71 @@
+
+
+// var a = 10;
+// console.log(a);
+
+// var a = true;
+// console.log(a);
+// --------------------
+// var a = 10;
+// console.log(a);
+
+// a = true;
+// console.log(a);
+
+// ------------------------
+
+// let a = 10;
+// console.log(a);
+
+// let a = true;
+// console.log(a);
+
+// ------------------------
+
+// let a = 10;
+// console.log(a);
+
+// a = true;
+// console.log(a);
+
+// ------------------------
+
+// const a = 10;
+// const a = true;
+
+// ------------------------
+
+// const a = 10;
+// a = true;
+
+// ------------------------
+
+// const a = 10;
+// a = true;
+
+
+// var a = 10;
+// const a = 200;
+// const a = 200;
+
+// ----------------------------------
+
+// eg:
+// 1.
+// var a = 10;
+// var a = 20;
+// console.log(a);
+
+// 2.
+// var a = 20;
+// let a = 300;
+// console.log(a);
+
+// 3. 
+// const a = 10;
+// console.log(a);
+// a=500
+ 
+// 4.
+// let a = 10;
+// var a = 20;
