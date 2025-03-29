9b1f604e07b4b7cb8e1e9695f63c3b8ded86f13d
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-03-29 21:11:36 +0530

    lecture 16 commit

---
+++ Lecture-16/scopes/app.js
@@ -0,0 +1,181 @@
+
+
+
+// var a = 10;     // global
+// let b = 20;     // script
+// const c = 30;   // script
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// if(true){
+//     var d = 100;    // global
+//     let e = 200;    // block
+//     const f = 300;  // block
+//     console.log(d);
+//     console.log(e);
+//     console.log(f);
+// }
+
+// function sam(){
+//     var g = 1000;   // functional  = local
+//     let h = 2000;   // block
+//     const i = 3000; // block
+//     console.log(g);
+//     console.log(h);
+//     console.log(i);
+// }
+// sam();
+
+
+// ---------------------------------------
+
+
+
+
+// function sam(){
+//     console.log(a);
+//     var a = 100;
+//     console.log(a);
+//     let b = 20;
+// }
+
+// sam();
+
+// ---------------------
+
+
+// let b = 20;
+// console.log(b);
+
+// function sam(){
+//     console.log(a);
+//     var a = 100;
+//     console.log(a);
+   
+// }
+// console.log(a);
+// console.log(b);
+
+// sam();
+
+
+// ---------------------
+
+
+
+// let b = 20;
+// console.log(b);
+
+// function sam(){
+//     console.log(a);
+//     var a = 100;
+//     console.log(a);
+// }
+// console.log(b);
+// sam();
+// console.log(a);
+
+
+// // ---------------------
+
+// let b = 20;
+// console.log(b);
+
+// if(true){
+//     console.log(a);
+//     var a = 100;
+//     console.log(a);
+// }
+// console.log(b);
+
+// console.log(a);
+
+
+// ---------------------
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var a = 10;
+// let b = 20;
+// const c = 30;
+
+// --------------------------------
+
+
+// console.log(a);
+
+// var a = 10;     //global
+
+// function sam(){
+//     console.log(a);
+//     var a = 20;    //functional
+//     console.log(a);
+// }
+// if(true){
+//     console.log(a);
+//     var a = 200; //global
+//     console.log(a); 
+// }
+// console.log(a);
+// sam();
+// console.log(a);
+
+
+
+// --------------------------------
+
+
+// shadowing (5 mins read)
+
+// a()
+// var a = 10;
+
+// function a(){
+//     console.log("bhai");
+// }
+
+// a()
+
+// --------------------------
+
+
+
+// console.log(a);
+
+// var a = 10;          
+
+// function sam(){
+//     console.log(a);
+// }
+// if(true){
+//     let a = 500;      
+//     console.log(a);
+//     var a = 200;     
+//     console.log(a); 
+// }
+// console.log(a);
+// sam();
+// console.log(a);
+
+
+
+// let a = 500;      
+// console.log(a);
+// var a = 200; 
+// (exceptionally zaroor dekho)
+
+// ----------------------------------
+
+
+function abc(){
+    var a = 10;
+    console.log(a);
+    let a = 20;
+    console.log(a); 
+}
+
+abc()
+
